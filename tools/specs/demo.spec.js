fe23a367260d5e35221b5de2b0c97ad9c4240311
Dzmitry Herasimov <timonthelure@gmail.com>
2018-01-30 14:40:04 +0300

    Fix npm scripts

---
+++ tools/specs/demo.spec.js
@@ -0,0 +1,9 @@
+describe('js-testing-workshop', function () {
+    describe('show test', function () {
+        it('should demonstrate that unit tests are easy', function () {
+            const tests = 'easy';
+
+            expect(tests).to.be.equal('easy');
+        });
+    });
+});
