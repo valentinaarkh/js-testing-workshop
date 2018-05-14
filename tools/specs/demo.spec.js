0c5e1da5c467c4d4b195163ed15baa4c18b60f47
Dzmitry Herasimov <timonthelure@gmail.com>
2018-05-14 10:27:31 +0300

    Update tasks and git ignore

--- tools/specs/demo.spec.js
+++
@@ -1,9 +0,0 @@
-describe('js-testing-workshop', function () {
-    describe('show test', function () {
-        it('should demonstrate that unit tests are easy', function () {
-            const tests = 'easy';
-
-            expect(tests).to.be.equal('easy');
-        });
-    });
-});
